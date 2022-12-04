6b770b03d985cf6f3edcf4197dd783573ce1f629
chanboklee <chanboklee@naver.com>
2022-12-04 17:44:35 +0900

    Fix: gradle 설정 변경 및 Querydsl 심플 테스트

---
+++ src/test/java/com/lee/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,73 @@
+package com.lee.querydsl;
+
+import com.lee.querydsl.entity.Member;
+import com.lee.querydsl.entity.QMember;
+import com.lee.querydsl.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        // search member1
+        // runtime 시점에 오류를 알 수 있어 사용자가 사용할 때 알 수 있음
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){
+        queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        // 컴파일 시점에 오류를 알 수 있다.
+        // 파라미터 바인딩을 해준다.
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
