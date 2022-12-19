312b676337e5fca7b0cc0505b6a17c021075b4de
chanboklee <chanboklee@naver.com>
2022-12-19 22:04:54 +0900

    Fix: Querydsl Convert to Spring Data JPA

--- src/main/java/com/lee/querydsl/QuerydslApplication.java
+++ src/main/java/com/lee/querydsl/QuerydslApplication.java
@@ -1,10 +1,20 @@
 package com.lee.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
+
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em){
+        return new JPAQueryFactory(em);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QuerydslApplication.class, args);
---
+++ src/main/java/com/lee/querydsl/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.lee.querydsl.repository;
+
+import com.lee.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
+}
