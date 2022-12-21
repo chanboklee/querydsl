1d3ea6f5edb68df31057adb75eca60da5427e590
chanboklee <chanboklee@naver.com>
2022-12-22 00:55:40 +0900

    Fix: Querydsl QuerydslPredicateExecutor..

--- src/test/java/com/lee/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/com/lee/querydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.lee.querydsl.repository;
 import com.lee.querydsl.dto.MemberSearchCondition;
 import com.lee.querydsl.dto.MemberTeamDto;
 import com.lee.querydsl.entity.Member;
+import com.lee.querydsl.entity.QMember;
 import com.lee.querydsl.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -89,6 +90,32 @@ class MemberRepositoryTest {
         Page<MemberTeamDto> result = memberRepository.searchPageSimple(memberSearchCondition, pageRequest);
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    // 조인이 안된다..
+    // 실무환경에서 사용하기에는 한계가 있다..
+    @Test
+    public void guerydslPredicateExecutorTest(){
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("member1 = " + findMember);
+        }
     }
 
 }
